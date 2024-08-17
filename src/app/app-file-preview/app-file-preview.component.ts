c6ec2484cce0df01235be0a11603318e44e97255
manuel <manuel.chirre@materiagris.pe>
2024-08-17 09:38:53 -0500

    cambios

--- src/app/app-file-preview/app-file-preview.component.ts
+++ src/app/app-file-preview/app-file-preview.component.ts
@@ -1,6 +1,7 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 import { DomSanitizer } from '@angular/platform-browser';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-app-file-preview',
@@ -34,29 +35,63 @@ export class AppFilePreviewComponent implements OnInit {
 
   }
 
-  descargarArchivo() {
-    console.log(this.fileData.sUrl)
+  // descargarArchivo() {
+  //   console.log(this.fileData.sUrl)
 
-    const headers = new HttpHeaders({
-      'Content-Type': 'application/json',
-      'Access-Control-Allow-Origin': 'http://localhost:4200', 
-      'Authorization': 'undefined',
-    });
+  //   const headers = new HttpHeaders().set(
+  //     "Accept",
+  //     "image/webp,*/*,application/pdf"
+  //   );
 
-    this.http.get(this.fileData.sUrl, { headers, responseType: 'blob' })
-    .subscribe(
-      (data: Blob) => {
-        const blob = new Blob([data], { type: 'application/pdf' });
+  //   this.http.get(this.fileData.sUrl, { headers, responseType: 'blob' as "json" })
+  //   .subscribe(
+  //     (data: Blob) => {
+  //       const blob = new Blob([data], { type: 'application/pdf' });
+  //       const link = document.createElement('a');
+  //       link.href = window.URL.createObjectURL(blob);
+  //       link.download = this.fileData.sFile_Name;
+  //       link.click();
+  //       window.URL.revokeObjectURL(link.href);
+  //     },
+  //     (error) => {
+  //       console.error('Error en la solicitud:', error);
+  //     }
+  //   );
+  // }
+
+
+  descargarAdjunto(sUrl: any) {
+    this.obtenerArchivo(sUrl).subscribe(
+      (blob: any) => {
         const link = document.createElement('a');
         link.href = window.URL.createObjectURL(blob);
         link.download = this.fileData.sFile_Name;
         link.click();
         window.URL.revokeObjectURL(link.href);
       },
-      (error) => {
-        console.error('Error en la solicitud:', error);
-      }
+      (error) => {}
     );
+  }
+
+  obtenerArchivo(sUrl: any): Observable<any> {
+    console.log('llego aqui')
+    let httpHeaders = new HttpHeaders().set(
+      "Accept",
+      "image/webp,*/*,application/pdf"
+    );
+    return this.http.get<Blob>(sUrl, {
+      headers: httpHeaders,
+      responseType: "blob" as "json",
+    }); // revisar si cuando descarga pdf tambien lo toma como blob
+  }
+
+  getPdf() {
+
+    const httpOptions = {
+      responseType: 'blob' as 'json'
+    };
+  
+    return this.http.get(this.fileData.sUrl, httpOptions);
   }
 
 }
